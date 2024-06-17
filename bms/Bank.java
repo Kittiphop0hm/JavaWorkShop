975364727b3c78bbeb6d05ffb95daad7e2affc8f
KittiphopOhm <kittiphop.sang@mail.kmutt.ac.th>
2024-06-17 17:12:47 +0700

    Finish Bank

--- bms/Bank.java
+++ bms/Bank.java
@@ -4,18 +4,46 @@ import java.util.ArrayList;
 
 public class Bank {
     private String bankName;
-    ArrayList<Account> arrayList = new ArrayList<>();
+    ArrayList<Account> arrayList = new ArrayList<Account>();
 
     public Bank(String bankName) {
         this.bankName = bankName;
     }
 
-    public void listAccount() {}
-    public void openAccount(Account account) {}
-    public void closeAccount(Account account) {}
-    public void depositMoney(Account account, double amount) {}
-    public void withdrawMoney(Account account, double amount) {}
+    public void listAccounts() {
+        for (Account account : arrayList) {
+            System.out.println(account);
+        }
+    }
+
+    public void openAccount(Account account) {
+        if (account == null) throw new RuntimeException();
+        arrayList.add(account);
+    }
+
+    public void closeAccount(Account account) {
+        if (account == null) throw new RuntimeException();
+        arrayList.remove(account);
+    }
+
+    public void depositMoney(Account account, double amount) {
+        if (account == null || amount <= 0) throw new RuntimeException();
+        account.deposit(amount);
+    }
+
+    public void withdrawMoney(Account account, double amount) {
+        if (account == null || amount <= 0 ) throw new RuntimeException();
+        account.withdraw(amount);
+    }
+
     public Account getAccount(int accountNumber, String accountType) {
+        if (accountNumber >= 0 || accountType != null) {
+            for (Account arraysAccount : arrayList) {
+                if (arraysAccount.getAccountNumber() == accountNumber && arraysAccount.getAccountType().equals(accountType)) {
+                    return arraysAccount;
+                }
+            }
+        }
         return null;
     }
 }
